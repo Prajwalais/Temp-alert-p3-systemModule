61ff3f21bfbc2d348b95221bd723f5aa7d56409b
Prajwalais <prajwalbakaningi7@gmail.com>
2025-11-27 14:47:18 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv)==2:
+  script_name=sys.argv[0]
+  temp=sys.argv[1]
+else:
+  script_name=sys.argv[0]
+  temp=100
+print(f"script_name: {script_name)\nTempeture: {temp}")
+
+if temp <15:
+  print(f"Cold")
+
+elif temp <30:
+  print(f"normal")
+  else
+  print(f"Hot")
