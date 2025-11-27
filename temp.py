03dac0d99a20930eeda0c8ad75d0b80caa22ec10
Prajwalais <prajwalbakaningi7@gmail.com>
2025-11-27 14:57:40 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,17 +1,17 @@
 import sys
 
-if len(sys.argv)==2:
-  script_name=sys.argv[0]
-  temp=float(sys.argv[1])
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    temp = int(sys.argv[1])
 else:
-  script_name=sys.argv[0]
-  temp=100
-print(f"script_name: {script_name}\nTempeture: {temp}")
+    script_name = sys.argv[0]
+    temp = 100
 
-if temp <15:
-  print(f"Cold")
+print(f"script_name: {script_name}\nTemperature: {temp}")
 
-elif temp <30:
-  print(f"normal")
-  else
-  print(f"Hot")
+if temp < 15:
+    print("Cold")
+elif temp < 30:
+    print("Normal")
+else:
+    print("Hot")
