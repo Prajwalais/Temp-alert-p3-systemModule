389607089ebdcfa13353f36330d01a7c8b481f01
Prajwalais <prajwalbakaningi7@gmail.com>
2025-11-27 15:17:31 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,17 +1,20 @@
 import sys
 
-if len(sys.argv) == 2:
-    script_name = sys.argv[0]
-    temp = int(sys.argv[1])
+if len(sys.argv) > 1:
+    numbers = list(map(int, sys.argv[1:]))
 else:
-    script_name = sys.argv[0]
-    temp = 100
 
-print(f"script_name: {script_name}\nTemperature: {temp}")
+    nums = input("Enter numbers separated by space: ")
+    numbers = list(map(int, nums.split()))
 
-if temp < 15:
-    print("Cold")
-elif temp < 30:
-    print("Normal")
-else:
-    print("Hot")
+even_count = 0
+odd_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print(f"Total Even Numbers: {even_count}")
+print(f"Total Odd Numbers: {odd_count}")
